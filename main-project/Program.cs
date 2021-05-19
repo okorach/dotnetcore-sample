eeb6262d1652a1a4a14fa9d73675214747775008
Olivier Korach <olivier.korach@gmail.com>
2021-05-19 15:44:56 +0200

    Add issues

--- main-project/Program.cs
+++ main-project/Program.cs
@@ -6,7 +6,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            var c1 = new MyClass();
+            var c1 = new MyClass(); // FIXME this is bad
+            var ip = "10.10.0.1";
             Console.WriteLine($"Hello World! {c1.ReturnMessage()}");
         }
     }
